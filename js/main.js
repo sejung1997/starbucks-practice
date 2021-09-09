89e4d2ebcbd79c376222cdd9888a094080d388c9
sejung1997 <ksgve1997814@naver.com>
2021-09-10 01:35:59 +0900

    gsap 적용

--- js/main.js
+++ js/main.js
@@ -12,5 +12,25 @@ searchInputEl.addEventListener('focus', function () {
 
 searchInputEl.addEventListener('blur', function () {
   searchEl.classList.remove('focused');
-  searchInputEl.setAttribute('placeholder', '');
+  searchInputEl.setAttribute('placeholder', '');  
 });
+
+const badgeEl = document.querySelector('header, badges');
+
+window.addEventListener('scroll', _.throttle(function () {
+  console.log(window.scrollY);
+  if (window.scrollY > 500) {
+    // gsap.to(요소, 지속시간, 옵션);
+    gsap.to(badgeEl, .6, {
+      opacity: 0,
+      display: 'none'
+    });
+  } else {
+    gsap.to(badgeEl, .6, {
+      opacity: 1,
+      display: 'block'
+    });
+
+  }
+}, 300)); // 3초`
+// _.throttle(함수, 시간)
