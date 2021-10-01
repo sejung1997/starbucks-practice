b8bc0e5bd5851ea32f68cb4fa7d888e0472ab6c2
sejung1997 <ksgve1997814@naver.com>
2021-10-01 15:30:32 +0900

    21

--- js/main.js
+++ js/main.js
@@ -14,36 +14,69 @@ searchInputEl.addEventListener('blur', function () {
   searchInputEl.setAttribute('placeholder', '')
 })
 
-const badgeEl = document.querySelector('header .badges')
-
+const badgesEl = document.querySelector('header .badges')
 window.addEventListener('scroll', _.throttle(function () {
   if(window.scrollY > 500) {
-    gsap.to(badgeEl, .6, {
+    gsap.to(badgesEl, .6, {
       opacity: 0,
-      display: 'none'
+      display:'none'
+      
     })
   }
-  else {
-    gsap.to(badgeEl, .6, {
-      opacity: 1,
-      display: 'block'
+  else{
+    gsap.to(badgesEl, .6, {
+      opacity:1,
+      display:'block'
     })
   }
-}, 300)
-) 
+}, 300))
 
-const fadeEls = document.querySelectorAll('.visual .fade-in')
-fadeEls.forEach(function (fadeEl, index) {
-  gsap.to(fadeEl, 1, {
+const visualEls = document.querySelectorAll('.visual .fade-in')
+visualEls.forEach( (visualEl, index) => {
+  gsap.to(visualEl, 1, {
     opacity: 1,
-    delay: (index + 1) * .7,
-
+    delay: (index + 1) * .7
   })
+});
+
+new Swiper('.notice-line .swiper-container', {
+  direction: 'vertical',
+  loop: true,
+  autoplay: true
 })
 
-const swiper = new Swiper('.notice-line .swiper-container', {
-  direction: 'vertical',
-  autoplay: true,
-  loop: true
+new Swiper('.promotion .swiper-container', {
+  slidesPerView: 3,
+  spaceBetween: 10,
+  centeredSlides: true,
+  loop:true,
+  autoplay: {
+    delay: 5000
+  },
+  pagination: {
+    el: '.promotion .swiper-pagination',
+    clickable: true,
+
+  },
+  navigation: {
+    prevEl: '.promotion .swiper-prev',
+    nextEl: '.promotion .swiper-next'
+  }
+
+
+})
+
+const promotionEl = document.querySelector('.promotion')
+const togglePromotionEl = document.querySelector('.toggle-promotion')
+let  isHidePromotion = false
+
+togglePromotionEl.addEventListener('click', function () {
+  isHidePromotion = !isHidePromotion
+  if(isHidePromotion) {
+    promotionEl.classList.add('hide')
+  }
+  else {
+    promotionEl.classList.remove('hide')
+  }
 })
 
